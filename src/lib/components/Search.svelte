9adc0e3f24e5225bc937f0408e97a5ec185f1143
th3hate <mo7iballah@gmail.com>
2023-01-12 10:56:08 +0200

    Added Add invoice slide panel

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -5,7 +5,7 @@
 <div class="relative flex w-full items-baseline mt-auto">
   <div class="mr-2 text-blue-700"><SearchIcon width={24} height={24} /></div>
   <input
-    class="w-full md:w-[14rem] lg:w-72 border-b-2 border-dashed border-b-blue-700 bg-transparent font-sansSerif text-base lg:text-xl text-black outline-none focus:border-solid focus:border-b-blue-500 "
+    class="search w-full md:w-[14rem] lg:w-72 border-b-2 border-dashed border-b-blue-700 bg-transparent font-sansSerif text-base lg:text-xl text-black outline-none focus:border-solid focus:border-b-blue-500 "
     type="text"
     name="search"
     placeholder="Invoice"
